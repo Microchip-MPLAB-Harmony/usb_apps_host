9bdd6fced0f5c4eb6d457bb25d7bec1dd076fbd9
Shijas Mayan <shijas.mayan@microchip.com>
2021-06-03 02:42:52 +0530

    [APPS]: Regeneration of the demo msd_basic/firmware/sam_9x60_ek_freertos.X [MH3-53964].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/msd_basic/firmware/src/config/sam_9x60_ek_freertos/initialization.c
+++ apps/msd_basic/firmware/src/config/sam_9x60_ek_freertos/initialization.c
@@ -303,6 +303,7 @@ static void SYSC_Disable( void )
 
 void SYS_Initialize ( void* data )
 {
+
 	SYSC_Disable( );
 
   
@@ -317,7 +318,7 @@ void SYS_Initialize ( void* data )
 
     MMU_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
