430fbbfb69ca054abab897ca54839df0394aa0bd
Shijas Mayan <shijas.mayan@microchip.com>
2021-06-03 00:35:18 +0530

    [APPS]: Regeneration of the demo cdc_basic/firmware/sam_9x60_ek_iar.IAR [MH3-53883].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/cdc_basic/firmware/src/config/sam_9x60_ek_iar/initialization.c
+++ apps/cdc_basic/firmware/src/config/sam_9x60_ek_iar/initialization.c
@@ -250,6 +250,7 @@ static void SYSC_Disable( void )
 
 void SYS_Initialize ( void* data )
 {
+
 	SYSC_Disable( );
 
 
@@ -263,7 +264,7 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
     MMU_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
--- apps/cdc_basic/firmware/src/config/sam_9x60_ek_iar/interrupts.h
+++ apps/cdc_basic/firmware/src/config/sam_9x60_ek_iar/interrupts.h
@@ -1,22 +1,22 @@
 /*******************************************************************************
-  DBGU PLIB
+ System Interrupts File
 
   Company:
     Microchip Technology Inc.
 
   File Name:
-    plib_dbgu.h
+    interrupt.h
 
   Summary:
-    DBGU PLIB Header File
+    Interrupt vectors mapping
 
   Description:
-    None
+    This file contains declarations of device vectors used by Harmony 3
+ *******************************************************************************/
 
-*******************************************************************************/
-
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -36,53 +36,26 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
-
-#ifndef PLIB_DBGU_H
-#define PLIB_DBGU_H
-
-#include "plib_dbgu_common.h"
-
-// DOM-IGNORE-BEGIN
-#ifdef __cplusplus  // Provide C++ Compatibility
-
-    extern "C" {
-
-#endif
+ *******************************************************************************/
 // DOM-IGNORE-END
 
+#ifndef INTERRUPTS_H
+#define INTERRUPTS_H
+
 // *****************************************************************************
 // *****************************************************************************
-// Section: Interface
+// Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-#define DBGU_FrequencyGet()    (uint32_t)(200000000UL)
+#include <stdint.h>
 
-/****************************** DBGU API *********************************/
 
-void DBGU_Initialize(void);
+// *****************************************************************************
+// *****************************************************************************
+// Section: Handler Routines
+// *****************************************************************************
+// *****************************************************************************
 
-DBGU_ERROR DBGU_ErrorGet(void);
 
-bool DBGU_SerialSetup(DBGU_SERIAL_SETUP *setup, uint32_t srcClkFreq);
 
-bool DBGU_Write(void *buffer, const size_t size);
-
-bool DBGU_Read(void *buffer, const size_t size);
-
-uint8_t DBGU_ReadByte(void);
-
-void DBGU_WriteByte(uint8_t data);
-
-bool DBGU_TransmitterIsReady(void);
-
-bool DBGU_ReceiverIsReady(void);
-
-// DOM-IGNORE-BEGIN
-#ifdef __cplusplus  // Provide C++ Compatibility
-
-    }
-
-#endif
-// DOM-IGNORE-END
-#endif // PLIB_DBGU_H
+#endif // INTERRUPTS_H
