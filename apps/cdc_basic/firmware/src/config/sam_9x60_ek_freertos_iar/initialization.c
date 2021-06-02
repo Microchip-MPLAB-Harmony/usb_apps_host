5a12e36dd4d0586be2cf9c3de160797a5689f65d
Shijas Mayan <shijas.mayan@microchip.com>
2021-06-03 00:34:39 +0530

    [APPS]: Regeneration of the demo cdc_basic/firmware/sam_9x60_ek_freertos_iar.IAR [MH3-53882].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/cdc_basic/firmware/src/config/sam_9x60_ek_freertos_iar/initialization.c
+++ apps/cdc_basic/firmware/src/config/sam_9x60_ek_freertos_iar/initialization.c
@@ -242,6 +242,7 @@ static void SYSC_Disable( void )
 
 void SYS_Initialize ( void* data )
 {
+
 	SYSC_Disable( );
 
 
@@ -257,7 +258,7 @@ void SYS_Initialize ( void* data )
 
     MMU_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
