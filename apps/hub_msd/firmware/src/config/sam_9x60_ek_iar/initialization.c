2a491aaf16f26d6e19230f07a6b773cd3593d019
Shijas Mayan <shijas.mayan@microchip.com>
2021-06-03 02:03:43 +0530

    [APPS]: Regeneration of the demo hub_msd/firmware/sam_9x60_ek_iar.IAR [MH3-53942].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/hub_msd/firmware/src/config/sam_9x60_ek_iar/initialization.c
+++ apps/hub_msd/firmware/src/config/sam_9x60_ek_iar/initialization.c
@@ -317,6 +317,7 @@ static void SYSC_Disable( void )
 
 void SYS_Initialize ( void* data )
 {
+
 	SYSC_Disable( );
 
   
@@ -329,7 +330,7 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
     MMU_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
     /* Disable WDT   */
     WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
