f940539806c29c2372b4f16dbc4abc1594195c7a
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 03:53:44 +0530

    [APPS]: Regenerating the demo msd_basic/firmware/sam_e70_xult_iar.IAR [MH3-62361].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/msd_basic/firmware/src/config/sam_e70_xult_iar/initialization.c
+++ apps/msd_basic/firmware/src/config/sam_e70_xult_iar/initialization.c
@@ -159,13 +159,15 @@ const SYS_FS_FUNCTIONS FatFsFunctions =
 };
 
 
+
 const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 {
     {
         .nativeFileSystemType = FAT,
         .nativeFileSystemFunctions = &FatFsFunctions
-    }
+    },
 };
+
 
 // </editor-fold>
 
