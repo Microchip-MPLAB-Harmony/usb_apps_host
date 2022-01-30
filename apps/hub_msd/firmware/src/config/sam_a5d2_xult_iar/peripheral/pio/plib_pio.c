d12caf11b6650c2e3b1ec34221e388346ec1ce32
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 03:09:32 +0530

    [APPS]: Regenerating the demo hub_msd/firmware/sam_a5d2_xult_iar.IAR [MH3-62329].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/hub_msd/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
+++ apps/hub_msd/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
@@ -60,6 +60,8 @@ void PIO_Initialize ( void )
 	PIOA_REGS->PIO_MSKR = 0x80000000LU;
 	PIOA_REGS->PIO_CFGR = 0x0U;
 	
+ /* Port A Latch configuration */
+	PIOA_REGS->PIO_CODR = 0x80000000LU;
 
  /* Port B Peripheral function GPIO configuration */
 	PIOB_REGS->PIO_MSKR = 0x661U;
@@ -81,6 +83,8 @@ void PIO_Initialize ( void )
 	PIOB_REGS->PIO_MSKR = 0x400U;
 	PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 	
+ /* Port B Latch configuration */
+	PIOB_REGS->PIO_CODR = 0x661U;
 
 
  /* Port D Peripheral function A configuration */
@@ -95,6 +99,8 @@ void PIO_Initialize ( void )
 	PIOD_REGS->PIO_MSKR = 0x800000U;
 	PIOD_REGS->PIO_CFGR = (PIOD_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 	
+ /* Port D Latch configuration */
+	PIOD_REGS->PIO_CODR = 0x800000U;
 
 
 
