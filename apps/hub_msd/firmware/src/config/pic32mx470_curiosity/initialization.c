8d3184be52d63acd74bab3d47dbaa0bf665d6c28
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 02:58:15 +0530

    [APPS]: Regenerating the demo hub_msd/firmware/pic32mx470_curiosity.X [MH3-62321].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/hub_msd/firmware/src/config/pic32mx470_curiosity/initialization.c
+++ apps/hub_msd/firmware/src/config/pic32mx470_curiosity/initialization.c
@@ -174,13 +174,15 @@ const SYS_FS_FUNCTIONS FatFsFunctions =
 };
 
 
+
 const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 {
     {
         .nativeFileSystemType = FAT,
         .nativeFileSystemFunctions = &FatFsFunctions
-    }
+    },
 };
+
 
 // </editor-fold>
 
@@ -218,6 +220,7 @@ const DRV_USBFS_INIT drvUSBFSInit =
 
 	/* Interrupt Source for USB module */
 	.interruptSource = INT_SOURCE_USB,
+
 
     
 	/* USB Controller to operate as USB Host */
