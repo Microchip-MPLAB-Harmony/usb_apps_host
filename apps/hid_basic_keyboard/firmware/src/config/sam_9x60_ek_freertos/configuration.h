e95c90839f61321ce81c299c69622713dcb6d8e4
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 02:13:44 +0530

    [APPS]: Regenerating the demo hid_basic_keyboard/firmware/sam_9x60_ek_freertos.X [MH3-62289].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/hid_basic_keyboard/firmware/src/config/sam_9x60_ek_freertos/configuration.h
+++ apps/hid_basic_keyboard/firmware/src/config/sam_9x60_ek_freertos/configuration.h
@@ -56,7 +56,7 @@
 */
 
 #include "user.h"
-#include "toolchain_specifics.h"
+#include "device.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -161,7 +161,7 @@ extern "C" {
 /* Number of Endpoints used */
 
 /* Total number of devices to be supported */
-#define USB_HOST_DEVICES_NUMBER                             1
+#define USB_HOST_DEVICES_NUMBER                             1 
 
 /* Target peripheral list entries */
 #define  USB_HOST_TPL_ENTRIES                               1 
