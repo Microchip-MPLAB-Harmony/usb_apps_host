4f220412a47206135ddd28c8f5d7622ef24e7dff
Shijas Mayan <shijas.mayan@microchip.com>
2022-01-31 03:20:10 +0530

    [APPS]: Regenerating the demo msd_basic/firmware/pic32mz_das_sk.X [MH3-62337].
    Signed-off-by: Shijas Mayan <shijas.mayan@microchip.com>

--- apps/msd_basic/firmware/src/config/pic32mz_das_sk/peripheral/gpio/plib_gpio.c
+++ apps/msd_basic/firmware/src/config/pic32mz_das_sk/peripheral/gpio/plib_gpio.c
@@ -57,32 +57,23 @@
 */
 void GPIO_Initialize ( void )
 {
-    /* PORTA Initialization */
 
+    /* PORTA Initialization */
     /* PORTB Initialization */
     LATB = 0x0; /* Initial Latch Value */
     TRISBCLR = 0x20; /* Direction Control */
     ANSELBCLR = 0x7020; /* Digital Mode Enable */
     CNPUBSET = 0x7000; /* Pull-Up Enable */
-
     /* PORTC Initialization */
-
     /* PORTD Initialization */
-
     /* PORTE Initialization */
-
     /* PORTF Initialization */
-
     /* PORTG Initialization */
-
     /* PORTH Initialization */
     LATH = 0x0; /* Initial Latch Value */
     TRISHCLR = 0x7; /* Direction Control */
-
     /* PORTJ Initialization */
-
     /* PORTK Initialization */
-
 
 
     /* PPS Input Remapping */
