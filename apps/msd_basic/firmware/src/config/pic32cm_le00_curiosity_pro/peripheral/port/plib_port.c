e37ff18293b079db2892a33e32a77dc16dd50d4d
Shijas Mayan <shijas.mayan@microchip.com>
2022-07-06 14:38:20 +0530

    [Apps] PIC32CM Lx USB Host demo configurations are regenerated with MCC to upgrade the DFPs to v1.1.62.[MH3-69507]

--- apps/msd_basic/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
+++ apps/msd_basic/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -89,9 +88,13 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[2].PORT_PINCFG[27] = 0x0;
    PORT_REGS->GROUP[2].PORT_PINCFG[28] = 0x6;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[10] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[13] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[14] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
