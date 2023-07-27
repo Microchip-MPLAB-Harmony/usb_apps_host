ef5b5c4e1859332440420f35b0f9894d35192bc5
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 13:37:52 +0530

    [Apps.] Regenerated demo for hid_basic_mouse_usart_sam_a5d27_wlsom1_ek1_freertos
    MH3-82450

--- apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d27_wlsom1_ek1_freertos/interrupts.h
+++ apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d27_wlsom1_ek1_freertos/interrupts.h
@@ -57,6 +57,13 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void UART0_InterruptHandler (void);
+void TC0_InterruptHandler (void);
+void UHPHS_Handler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
