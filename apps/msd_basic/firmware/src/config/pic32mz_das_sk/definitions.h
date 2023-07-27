493169e97d76e2941f818c59d6d2ad2bd0d4671f
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 15:03:49 +0530

    [Apps.] Regenerated demo for msd_basic_pic32mz_das_sk
    MH3-82489

--- apps/msd_basic/firmware/src/config/pic32mz_das_sk/definitions.h
+++ apps/msd_basic/firmware/src/config/pic32mz_das_sk/definitions.h
@@ -83,6 +83,12 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MZ2064DAS169"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MZDA"
+#define DEVICE_SERIES		 "PIC32MZ"
+
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 200000000
 
@@ -198,7 +204,7 @@ typedef struct
 {
 	SYS_MODULE_OBJ  drvUSBHSObject;
 
-	SYS_MODULE_OBJ  usbHostObject0;
+    SYS_MODULE_OBJ  usbHostObject0;
 
     SYS_MODULE_OBJ  sysTime;
 
