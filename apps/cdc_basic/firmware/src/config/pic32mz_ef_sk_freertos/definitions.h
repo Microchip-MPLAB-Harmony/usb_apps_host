ae922dd4b92853ea2600be2ec7a334afd7c49191
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 12:56:05 +0530

    [Apps.] Regenerated demo for cdc_basic_pic32mz_ef_sk_freertos
    MH3-82387

--- apps/cdc_basic/firmware/src/config/pic32mz_ef_sk_freertos/definitions.h
+++ apps/cdc_basic/firmware/src/config/pic32mz_ef_sk_freertos/definitions.h
@@ -48,16 +48,16 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/coretimer/plib_coretimer.h"
-#include "driver/usb/usbhs/drv_usbhs.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "peripheral/coretimer/plib_coretimer.h"
+#include "driver/usb/usbhs/drv_usbhs.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
+#include "bsp/bsp.h"
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
-#include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "system/int/sys_int.h"
@@ -75,6 +75,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MZ2048EFH144"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MZEF"
+#define DEVICE_SERIES		 "PIC32MZ"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 200000000
@@ -189,9 +195,9 @@ Remarks:
 
 typedef struct
 {
-	SYS_MODULE_OBJ  drvUSBHSObject;
+    SYS_MODULE_OBJ  usbHostObject0;
 
-	SYS_MODULE_OBJ  usbHostObject0;
+	SYS_MODULE_OBJ  drvUSBHSObject;
 
     SYS_MODULE_OBJ  sysTime;
 
