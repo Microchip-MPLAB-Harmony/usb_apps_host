35f9cef216964abe0a8736607fe29668c420028e
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 12:44:30 +0530

    [Apps.] Regenerated demo for msd_basic_pic32mx470_curiosity
    MH3-82487

--- apps/msd_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
+++ apps/msd_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
@@ -48,9 +48,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_msd.h"
 #include "usb/usb_host_msd.h"
 #include "usb/usb_host_scsi.h"
@@ -80,6 +80,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MX470F512H"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MX1185"
+#define DEVICE_SERIES		 "PIC32MX"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 96000000
@@ -194,7 +200,7 @@ Remarks:
 
 typedef struct
 {
-	SYS_MODULE_OBJ  usbHostObject0;
+    SYS_MODULE_OBJ  usbHostObject0;
 
 	SYS_MODULE_OBJ  drvUSBFSObject;
 
