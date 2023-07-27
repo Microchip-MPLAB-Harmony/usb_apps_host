5ca82870d182550a9df043c09f055ba715f94008
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 14:28:19 +0530

    [Apps.] Regenerated demo for cdc_basic_sam_e70_xult_freertos
    MH3-82403

--- apps/cdc_basic/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/cdc_basic/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -61,11 +61,11 @@
 #include "peripheral/tc/plib_tc0.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -77,6 +77,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAME70Q21B"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAME"
+#define DEVICE_SERIES		 "SAME70"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 300000000
@@ -193,7 +199,7 @@ typedef struct
 {
 	SYS_MODULE_OBJ  drvUSBHSV1Object;
 
-	SYS_MODULE_OBJ  usbHostObject0;
+    SYS_MODULE_OBJ  usbHostObject0;
 
     SYS_MODULE_OBJ  sysTime;
 
