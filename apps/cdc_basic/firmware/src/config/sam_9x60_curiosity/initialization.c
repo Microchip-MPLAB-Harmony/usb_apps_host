e585991c72c16ed85daa64dd249eca077c0c22e2
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-07-27 13:01:22 +0530

    [Apps.] Regenerated demo for cdc_basic_sam_9x60_curiosity
    MH3-82388

--- apps/cdc_basic/firmware/src/config/sam_9x60_curiosity/initialization.c
+++ apps/cdc_basic/firmware/src/config/sam_9x60_curiosity/initialization.c
@@ -287,15 +287,15 @@ void SYS_Initialize ( void* data )
     H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
         
     sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
-
+    
     /* MISRAC 2012 deviation block end */
-
-    /* Initialize the USB Host layer */
-    sysObj.usbHostObject0 = USB_HOST_Initialize (( SYS_MODULE_INIT *)& usbHostInitData );	
 
      /* Initialize USB Driver */ 
     sysObj.drvUSBEHCIObject = DRV_USB_EHCI_Initialize (DRV_USB_EHCI_INDEX_0, (SYS_MODULE_INIT *) &drvUSBEHCIInit);
     sysObj.drvUSBOHCIObject = DRV_USB_OHCI_Initialize (DRV_USB_OHCI_INDEX_0, (SYS_MODULE_INIT *) &drvUSBOHCIInit);
+
+    /* Initialize the USB Host layer */
+    sysObj.usbHostObject0 = USB_HOST_Initialize (( SYS_MODULE_INIT *)& usbHostInitData );	
 
 
     /* MISRAC 2012 deviation block end */
