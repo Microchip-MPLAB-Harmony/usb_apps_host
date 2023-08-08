1a97a66a0c1e3f23f4241c3097b2b8fa513e9c6f
Nagaraju Sreeram <nagaraju.sreeram@microchip.com>
2023-08-08 10:30:11 +0530

    [APPS]Enable exFAT and regenerated msd_basic demo for SAM_G55_xpro_freertos configuration.[MH3-84674]

--- apps/msd_basic/firmware/src/config/sam_g55_xpro_freertos/configuration.h
+++ apps/msd_basic/firmware/src/config/sam_g55_xpro_freertos/configuration.h
@@ -146,31 +146,6 @@ extern "C" {
 #define USB_HOST_MSD_LUN_NUMBERS              1
 
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: USB Host Layer Configuration
-// *****************************************************************************
-// **************************************************************************
-
-/* Number of Endpoints used */
-
-/* Total number of devices to be supported */
-#define USB_HOST_DEVICES_NUMBER                             1 
-
-/* Target peripheral list entries */
-#define  USB_HOST_TPL_ENTRIES                               1 
-
-/* Maximum number of configurations supported per device */
-#define USB_HOST_DEVICE_INTERFACES_NUMBER                   5    
-
-#define USB_HOST_CONTROLLERS_NUMBER                         1
-
-#define USB_HOST_TRANSFERS_NUMBER                           10
-
-/* Provides Host pipes number */
-#define USB_HOST_PIPES_NUMBER                               10
-
-
 
 /* EHCI Driver is not used */  
 #define DRV_USB_EHCI_INSTANCES_NUMBER                     0
@@ -198,6 +173,31 @@ extern "C" {
 #ifndef USB_ALIGN
 #define USB_ALIGN __ALIGNED(32)
 #endif 
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: USB Host Layer Configuration
+// *****************************************************************************
+// **************************************************************************
+
+/* Number of Endpoints used */
+
+/* Total number of devices to be supported */
+#define USB_HOST_DEVICES_NUMBER                             1 
+
+/* Target peripheral list entries */
+#define  USB_HOST_TPL_ENTRIES                               1 
+
+/* Maximum number of configurations supported per device */
+#define USB_HOST_DEVICE_INTERFACES_NUMBER                   5    
+
+#define USB_HOST_CONTROLLERS_NUMBER                         1
+
+#define USB_HOST_TRANSFERS_NUMBER                           10
+
+/* Provides Host pipes number */
+#define USB_HOST_PIPES_NUMBER                               10
+
 
 
 
