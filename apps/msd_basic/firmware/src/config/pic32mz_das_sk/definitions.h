e4497c43cd07987517190e32170915b6b5f9be8f
Nagaraju Sreeram <nagaraju.sreeram@microchip.com>
2023-08-09 11:34:57 +0530

    [APPS]Enable exFAT and regenerated msd_basic demo for pic32mz_das_sk configuration.[MH3-84696]

--- apps/msd_basic/firmware/src/config/pic32mz_das_sk/definitions.h
+++ apps/msd_basic/firmware/src/config/pic32mz_das_sk/definitions.h
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "driver/usb/usbhs/drv_usbhs.h"
+#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
 #include "usb/usb_msd.h"
@@ -66,11 +66,11 @@
 #include "peripheral/cache/plib_cache.h"
 #include "peripheral/evic/plib_evic.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
