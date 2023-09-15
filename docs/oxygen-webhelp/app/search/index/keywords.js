bca44d713487b36ca34503ca99c76e656ecfd471
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-09-15 16:12:06 +0530

    [Apps.]Updated Webhelp documents[MH3-85680]

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,20 @@
+define(function() {var keywords=[{w:"2",p:["p0"]},{w:"Microchip",p:["p0"]},{w:"MPLAB\u00AE",p:["p0"]},{w:"Harmony",p:["p0","p3"]},{w:"3",p:["p0","p3","p10"]},{w:"Release",p:["p0"]},{w:"Notes",p:["p0"]},{w:"1.1",p:["p1"]},{w:"CDC",p:["p1","p2","p4"]},{w:"Basic",p:["p1","p5","p9","p11"]},{w:"Example",p:["p1","p2","p4","p5","p6","p7","p8","p9","p11"]},{w:"(cdc_basic)",p:["p1"]},{w:"1.5",p:["p2"]},{w:"HUB",p:["p2","p6","p7"]},{w:"HID",p:["p2","p5","p9"]},{w:"(hub_cdc_hid)",p:["p2"]},{w:"1",p:["p3"]},{w:"USB",p:["p3"]},{w:"Host",p:["p3"]},{w:"application",p:["p3"]},{w:"examples",p:["p3"]},{w:"1.2",p:["p4"]},{w:"MSD",p:["p4","p6","p7","p11"]},{w:"(cdc_msd)",p:["p4"]},{w:"1.3",p:["p5"]},{w:"Keyboard",p:["p5"]},{w:"(hid_basic_keyboard)",p:["p5"]},{w:"1.8",p:["p6"]},{w:"Root",p:["p6"]},{w:"Dual",p:["p6"]},{w:"(root_hub_dual_msd)",p:["p6"]},{w:"1.6",p:["p7"]},{w:"(hub_msd)",p:["p7"]},{w:"1.9",p:["p8"]},{w:"Vendor",p:["p8"]},{w:"(vendor)",p:["p8"]},{w:"1.4",p:["p9"]},{w:"Mouse",p:["p9"]},{w:"USART",p:["p9"]},{w:"(hid_basic_mouse_usart)",p:["p9"]},{w:"License",p:["p10"]},{w:"1.7",p:["p11"]},{w:"(msd_basic)",p:["p11"]},{w:"Context",p:["p12"]},{w:"Sensitive",p:["p12"]},{w:"Help",p:["p12"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4, 5, 6];
+ph["p1"]=[7, 8, 9, 10, 11];
+ph["p2"]=[12, 13, 8, 14, 10, 15];
+ph["p3"]=[16, 3, 4, 17, 18, 19, 20];
+ph["p4"]=[21, 8, 22, 10, 23];
+ph["p5"]=[24, 14, 9, 25, 10, 26];
+ph["p6"]=[27, 28, 13, 29, 22, 10, 30];
+ph["p7"]=[31, 13, 22, 10, 32];
+ph["p8"]=[33, 34, 10, 35];
+ph["p9"]=[36, 14, 9, 37, 38, 10, 39];
+ph["p10"]=[4, 40];
+ph["p12"]=[43, 44, 45];
+ph["p11"]=[41, 22, 9, 10, 42];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
