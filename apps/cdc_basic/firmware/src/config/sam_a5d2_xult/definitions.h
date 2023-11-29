311f81f132fb79d1f94c317c19f283fc6a4e80c5
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-29 22:39:25 +0530

    [Apps.] Regenerated demo for cdc_basic_sam_a5d2_xult
    MH3-87589

--- apps/cdc_basic/firmware/src/config/sam_a5d2_xult/definitions.h
+++ apps/cdc_basic/firmware/src/config/sam_a5d2_xult/definitions.h
@@ -48,9 +48,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include "bsp/bsp.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
-#include "bsp/bsp.h"
 #include "driver/usb/uhp/drv_usb_ehci.h"
 #include "driver/usb/uhp/drv_usb_ohci.h"
 #include "peripheral/mmu/plib_mmu.h"
@@ -61,11 +61,11 @@
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
