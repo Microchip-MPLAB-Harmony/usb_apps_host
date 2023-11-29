2089e5c1a0a230ccaa8b2d256052958b6b9a142c
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-29 23:11:31 +0530

    [Apps.] Regenerated demo for vendor_sam_e54_xpro
    MH3-87707

--- apps/vendor/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/vendor/firmware/src/config/sam_e54_xpro/definitions.h
@@ -202,7 +202,7 @@ typedef struct
     SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  usbHostObject0;
 
-	SYS_MODULE_OBJ  drvUSBFSV1Object;
+    SYS_MODULE_OBJ  drvUSBFSV1Object;
 
     SYS_MODULE_OBJ  sysConsole0;
 
