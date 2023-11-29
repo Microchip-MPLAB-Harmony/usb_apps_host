611b2a642a945ec643b6b1b035db353411cfd65b
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-29 23:19:23 +0530

    [Apps.] Regenerated demo for hid_basic_mouse_usart_sam_e70_xult
    MH3-87645

--- apps/hid_basic_mouse_usart/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/hid_basic_mouse_usart/firmware/src/config/sam_e70_xult/definitions.h
@@ -54,10 +54,10 @@
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
+#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "bsp/bsp.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
-#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "driver/usart/drv_usart.h"
 #include "usb/usb_host_hid.h"
 #include "usb/usb_hid.h"
@@ -201,9 +201,9 @@ typedef struct
 {
     SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  drvUsart0;
-    SYS_MODULE_OBJ  usbHostObject0;
+    SYS_MODULE_OBJ  drvUSBHSV1Object;
 
-	SYS_MODULE_OBJ  drvUSBHSV1Object;
+    SYS_MODULE_OBJ  usbHostObject0;
 
 
 } SYSTEM_OBJECTS;
