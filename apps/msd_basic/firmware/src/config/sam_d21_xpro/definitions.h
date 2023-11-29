dd470e1e9c087cbdd5685a3ba92dfc785a4d1f40
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-11-29 23:02:13 +0530

    [Apps.] Regenerated demo for msd_basic_sam_d21_xpro
    MH3-87695

--- apps/msd_basic/firmware/src/config/sam_d21_xpro/definitions.h
+++ apps/msd_basic/firmware/src/config/sam_d21_xpro/definitions.h
@@ -205,7 +205,7 @@ typedef struct
     SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  usbHostObject0;
 
-	SYS_MODULE_OBJ  drvUSBFSV1Object;
+    SYS_MODULE_OBJ  drvUSBFSV1Object;
 
 
 } SYSTEM_OBJECTS;
