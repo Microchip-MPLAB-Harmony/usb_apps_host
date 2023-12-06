f561bf04cea54148af31c7fcfdad6ecfdf1e349e
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-06 18:45:17 +0530

    [Apps.] Regenerated demo for cdc_basic_pic32ck_gc01_curiosity_ultra
    MH3-87572

--- apps/cdc_basic/firmware/src/config/pic32ck_gc01_curiosity_ultra/definitions.h
+++ apps/cdc_basic/firmware/src/config/pic32ck_gc01_curiosity_ultra/definitions.h
@@ -61,11 +61,11 @@
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
 #include "peripheral/rtc/plib_rtc.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
