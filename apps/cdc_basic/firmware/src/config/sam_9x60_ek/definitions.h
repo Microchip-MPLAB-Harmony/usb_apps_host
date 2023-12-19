95bd099bb8fe2e58b13f325b76f0e84eb12c307b
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-20 00:43:44 +0530

    [Apps.] Regenerated demo for cdc_basic_sam_9x60_ek
    MH3-87582

--- apps/cdc_basic/firmware/src/config/sam_9x60_ek/definitions.h
+++ apps/cdc_basic/firmware/src/config/sam_9x60_ek/definitions.h
@@ -60,13 +60,13 @@
 #include "peripheral/aic/plib_aic.h"
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
-#include "peripheral/dbgu/plib_dbgu.h"
 #include "peripheral/tc/plib_tc0.h"
+#include "peripheral/dbgu/plib_dbgu.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
