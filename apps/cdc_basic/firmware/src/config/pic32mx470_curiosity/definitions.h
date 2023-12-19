138d20b39fdb3fa78cb1e32bd85389ded1a0953b
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-20 00:32:19 +0530

    [Apps.] Regenerated demo for cdc_basic_pic32mx470_curiosity
    MH3-87576

--- apps/cdc_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
+++ apps/cdc_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
@@ -48,9 +48,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "peripheral/coretimer/plib_coretimer.h"
 #include "bsp/bsp.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
@@ -58,10 +58,10 @@
 #include "driver/usb/usbfs/drv_usbfs.h"
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
