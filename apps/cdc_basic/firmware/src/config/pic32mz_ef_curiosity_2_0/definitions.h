2eae3a6ad81d58bc593d1b7dccb88ed2a1f3b82c
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-20 00:34:30 +0530

    [Apps.] Regenerated demo for cdc_basic_pic32mz_ef_curiosity_2_0
    MH3-87577

--- apps/cdc_basic/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
+++ apps/cdc_basic/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
@@ -48,20 +48,20 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/usbhs/drv_usbhs.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
 #include "peripheral/coretimer/plib_coretimer.h"
+#include "driver/usb/usbhs/drv_usbhs.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -193,9 +193,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  drvUSBHSObject;
-
     SYS_MODULE_OBJ  usbHostObject0;
+
+    SYS_MODULE_OBJ  drvUSBHSObject;
 
     SYS_MODULE_OBJ  sysTime;
 
