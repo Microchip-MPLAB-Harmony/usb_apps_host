a8e2122ef96415dd8aa9ac3a7c43af3aeebfdcac
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2023-12-20 00:34:53 +0530

    [Apps.] Regenerated demo for hub_cdc_hid_pic32mz_ef_curiosity_2_0
    MH3-87650

--- apps/hub_cdc_hid/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
+++ apps/hub_cdc_hid/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
@@ -48,11 +48,11 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
 #include "usb/usb_host_hub.h"
 #include "driver/usb/usbhs/drv_usbhs.h"
+#include "peripheral/coretimer/plib_coretimer.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
@@ -62,10 +62,10 @@
 #include "usb/usb_host_hid.h"
 #include "usb/usb_hid.h"
 #include "usb/usb_host_hid_mouse.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
