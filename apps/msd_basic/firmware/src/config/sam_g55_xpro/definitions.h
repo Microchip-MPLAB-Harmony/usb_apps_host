789884a362eaa78fbf4837b8adf752e6156b4b36
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 18:06:22 +0530

    [Apps.] Regenerated demo for msd_basic_sam_g55_xpro
    MH3-93434

--- apps/msd_basic/firmware/src/config/sam_g55_xpro/definitions.h
+++ apps/msd_basic/firmware/src/config/sam_g55_xpro/definitions.h
@@ -67,11 +67,11 @@
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
