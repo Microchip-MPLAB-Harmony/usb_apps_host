b95bc9773b40997ea519f56fb7d1230d255b8734
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-02 18:06:14 +0530

    [Apps.] Regenerated demo for hid_basic_mouse_usart_sam_g55_xpro
    MH3-93376

--- apps/hid_basic_mouse_usart/firmware/src/config/sam_g55_xpro/configuration.h
+++ apps/hid_basic_mouse_usart/firmware/src/config/sam_g55_xpro/configuration.h
@@ -109,31 +109,6 @@ extern "C" {
 // Section: Middleware & Other Library Configuration
 // *****************************************************************************
 // *****************************************************************************
-// *****************************************************************************
-// *****************************************************************************
-// Section: USB Host Layer Configuration
-// *****************************************************************************
-// **************************************************************************
-
-/* Number of Endpoints used */
-
-/* Total number of devices to be supported */
-#define USB_HOST_DEVICES_NUMBER                             1U
-
-/* Target peripheral list entries */
-#define  USB_HOST_TPL_ENTRIES                               1 
-
-/* Maximum number of configurations supported per device */
-#define USB_HOST_DEVICE_INTERFACES_NUMBER                   5    
-
-#define USB_HOST_CONTROLLERS_NUMBER                         1U
-
-#define USB_HOST_TRANSFERS_NUMBER                           10U
-
-/* Provides Host pipes number */
-#define USB_HOST_PIPES_NUMBER                               10U
-
-
 
 /* EHCI Driver is not used */  
 #define DRV_USB_EHCI_INSTANCES_NUMBER                     0U
@@ -161,6 +136,31 @@ extern "C" {
 #ifndef USB_ALIGN
 #define USB_ALIGN __ALIGNED(32)
 #endif 
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: USB Host Layer Configuration
+// *****************************************************************************
+// **************************************************************************
+
+/* Number of Endpoints used */
+
+/* Total number of devices to be supported */
+#define USB_HOST_DEVICES_NUMBER                             1U
+
+/* Target peripheral list entries */
+#define  USB_HOST_TPL_ENTRIES                               1 
+
+/* Maximum number of configurations supported per device */
+#define USB_HOST_DEVICE_INTERFACES_NUMBER                   5    
+
+#define USB_HOST_CONTROLLERS_NUMBER                         1U
+
+#define USB_HOST_TRANSFERS_NUMBER                           10U
+
+/* Provides Host pipes number */
+#define USB_HOST_PIPES_NUMBER                               10U
+
 
 /* Number of HID Client driver instances in the application */
 #define USB_HOST_HID_INSTANCES_NUMBER        1U
