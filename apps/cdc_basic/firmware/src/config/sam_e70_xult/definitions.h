eab932adf22ae035abdbf80a043847fca1eb6747
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-25 12:59:52 +0530

    [Apps.] Regenerated demo for cdc_basic_sam_e70_xult
    MH3-93316

--- apps/cdc_basic/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/cdc_basic/firmware/src/config/sam_e70_xult/definitions.h
@@ -49,9 +49,9 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "driver/usb/usbhsv1/drv_usbhsv1.h"
-#include "bsp/bsp.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "bsp/bsp.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
