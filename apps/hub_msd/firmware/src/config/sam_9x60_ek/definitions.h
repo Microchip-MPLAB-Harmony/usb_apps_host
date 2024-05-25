6b66608c6733b76a3425155046ccede9c08f7924
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-25 12:37:30 +0530

    [Apps.] Regenerated demo for hub_msd_sam_9x60_ek
    MH3-93391

--- apps/hub_msd/firmware/src/config/sam_9x60_ek/definitions.h
+++ apps/hub_msd/firmware/src/config/sam_9x60_ek/definitions.h
@@ -48,11 +48,11 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/uhp/drv_usb_ehci.h"
-#include "driver/usb/uhp/drv_usb_ohci.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
 #include "usb/usb_host_hub.h"
+#include "driver/usb/uhp/drv_usb_ehci.h"
+#include "driver/usb/uhp/drv_usb_ohci.h"
 #include "bsp/bsp.h"
 #include "usb/usb_msd.h"
 #include "usb/usb_host_msd.h"
@@ -204,10 +204,10 @@ Remarks:
 
 typedef struct
 {
+    SYS_MODULE_OBJ  usbHostObject0;
+
     SYS_MODULE_OBJ  drvUSBEHCIObject;
     SYS_MODULE_OBJ  drvUSBOHCIObject;
-
-    SYS_MODULE_OBJ  usbHostObject0;
 
     SYS_MODULE_OBJ  sysTime;
 
