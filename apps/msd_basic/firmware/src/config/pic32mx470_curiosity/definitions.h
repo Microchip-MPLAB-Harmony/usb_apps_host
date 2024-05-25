374621009c2925cf3acd265fc28d248a3c618afe
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-25 12:28:18 +0530

    [Apps.] Regenerated demo for msd_basic_pic32mx470_curiosity
    MH3-93409

--- apps/msd_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
+++ apps/msd_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
@@ -65,10 +65,10 @@
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
 #include "driver/usb/usbfs/drv_usbfs.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
