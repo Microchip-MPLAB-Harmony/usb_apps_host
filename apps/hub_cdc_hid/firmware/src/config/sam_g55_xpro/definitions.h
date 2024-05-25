1acdab66204db93a38e98f7f411302910d42f55e
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-25 13:03:05 +0530

    [Apps.] Regenerated demo for hub_cdc_hid_sam_g55_xpro
    MH3-93382

--- apps/hub_cdc_hid/firmware/src/config/sam_g55_xpro/definitions.h
+++ apps/hub_cdc_hid/firmware/src/config/sam_g55_xpro/definitions.h
@@ -59,16 +59,16 @@
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
+#include "peripheral/efc/plib_efc.h"
 #include "usb/usb_host_hid.h"
 #include "usb/usb_hid.h"
 #include "usb/usb_host_hid_mouse.h"
-#include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
