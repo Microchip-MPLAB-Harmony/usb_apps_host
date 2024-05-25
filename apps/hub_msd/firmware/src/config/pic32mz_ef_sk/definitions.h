0d24687e47e40f96e4158fd25d37ff323a263439
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-25 12:31:25 +0530

    [Apps.] Regenerated demo for hub_msd_pic32mz_ef_sk
    MH3-93388

--- apps/hub_msd/firmware/src/config/pic32mz_ef_sk/definitions.h
+++ apps/hub_msd/firmware/src/config/pic32mz_ef_sk/definitions.h
@@ -67,11 +67,11 @@
 #include "peripheral/cache/plib_cache.h"
 #include "peripheral/evic/plib_evic.h"
 #include "bsp/bsp.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
