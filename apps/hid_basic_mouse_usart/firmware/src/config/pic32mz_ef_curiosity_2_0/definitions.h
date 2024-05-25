684a0749a82b1e1e31095bd8a07ec9233ac68503
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-25 12:30:09 +0530

    [Apps.] Regenerated demo for hid_basic_mouse_usart_pic32mz_ef_curiosity_2_0
    MH3-93358

--- apps/hid_basic_mouse_usart/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
+++ apps/hid_basic_mouse_usart/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
@@ -60,12 +60,12 @@
 #include "usb/usb_host_hid.h"
 #include "usb/usb_hid.h"
 #include "usb/usb_host_hid_mouse.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/dma/sys_dma.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
 #include "peripheral/uart/plib_uart6.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
