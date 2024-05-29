a1767fefe607d58841c5e85736c4f4a76a1f2af0
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 15:41:30 +0530

    [Apps.] Regenerated demo for msd_basic_pic32mx470_curiosity
    MH3-93409

--- apps/msd_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
+++ apps/msd_basic/firmware/src/config/pic32mx470_curiosity/definitions.h
@@ -48,13 +48,13 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
-#include "bsp/bsp.h"
+#include "peripheral/coretimer/plib_coretimer.h"
 #include "usb/usb_msd.h"
 #include "usb/usb_host_msd.h"
 #include "usb/usb_host_scsi.h"
+#include "bsp/bsp.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
@@ -65,10 +65,10 @@
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
 #include "driver/usb/usbfs/drv_usbfs.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
