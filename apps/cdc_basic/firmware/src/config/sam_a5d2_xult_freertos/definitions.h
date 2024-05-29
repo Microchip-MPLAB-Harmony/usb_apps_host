c68f5275b79f41d8619c40a6aac15ca4ef46b4bb
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 16:09:22 +0530

    [Apps.] Regenerated demo for cdc_basic_sam_a5d2_xult_freertos
    MH3-93312

--- apps/cdc_basic/firmware/src/config/sam_a5d2_xult_freertos/definitions.h
+++ apps/cdc_basic/firmware/src/config/sam_a5d2_xult_freertos/definitions.h
@@ -48,11 +48,11 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/uhp/drv_usb_ehci.h"
-#include "driver/usb/uhp/drv_usb_ohci.h"
-#include "bsp/bsp.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "bsp/bsp.h"
+#include "driver/usb/uhp/drv_usb_ehci.h"
+#include "driver/usb/uhp/drv_usb_ohci.h"
 #include "peripheral/pit/plib_pit.h"
 #include "peripheral/mmu/plib_mmu.h"
 #include "peripheral/matrix/plib_matrix.h"
@@ -200,10 +200,10 @@ Remarks:
 
 typedef struct
 {
+    SYS_MODULE_OBJ  usbHostObject0;
+
     SYS_MODULE_OBJ  drvUSBEHCIObject;
     SYS_MODULE_OBJ  drvUSBOHCIObject;
-
-    SYS_MODULE_OBJ  usbHostObject0;
 
     SYS_MODULE_OBJ  sysTime;
 
