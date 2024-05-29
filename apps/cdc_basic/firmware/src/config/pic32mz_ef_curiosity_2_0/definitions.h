eaeb02c71ef9dfbae9710bcce46842e59f4ba949
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 15:43:48 +0530

    [Apps.] Regenerated demo for cdc_basic_pic32mz_ef_curiosity_2_0
    MH3-93297

--- apps/cdc_basic/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
+++ apps/cdc_basic/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
@@ -48,10 +48,10 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include "peripheral/coretimer/plib_coretimer.h"
+#include "driver/usb/usbhs/drv_usbhs.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
-#include "driver/usb/usbhs/drv_usbhs.h"
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/evic/plib_evic.h"
@@ -193,9 +193,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  usbHostObject0;
-
     SYS_MODULE_OBJ  drvUSBHSObject;
+
+    SYS_MODULE_OBJ  usbHostObject0;
 
     SYS_MODULE_OBJ  sysTime;
 
