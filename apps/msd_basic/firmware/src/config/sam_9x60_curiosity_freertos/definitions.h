41aad4b0e9a77e09b906d1ffef7c5fdd123f4f89
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 15:50:11 +0530

    [Apps.] Regenerated demo for msd_basic_sam_9x60_curiosity_freertos
    MH3-93417

--- apps/msd_basic/firmware/src/config/sam_9x60_curiosity_freertos/definitions.h
+++ apps/msd_basic/firmware/src/config/sam_9x60_curiosity_freertos/definitions.h
@@ -70,11 +70,11 @@
 #include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
