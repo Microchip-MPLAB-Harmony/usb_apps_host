bbfd95f91ebe6d455a7532348a436a98f5b65cf2
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-05-29 16:18:40 +0530

    [Apps.] Regenerated demo for msd_basic_sam_e70_xult_freertos
    MH3-93433

--- apps/msd_basic/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/msd_basic/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -57,10 +57,10 @@
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
+#include "bsp/bsp.h"
+#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
-#include "driver/usb/usbhsv1/drv_usbhsv1.h"
-#include "bsp/bsp.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
@@ -205,9 +205,9 @@ Remarks:
 typedef struct
 {
     SYS_MODULE_OBJ  sysTime;
-    SYS_MODULE_OBJ  usbHostObject0;
-
     SYS_MODULE_OBJ  drvUSBHSV1Object;
+
+    SYS_MODULE_OBJ  usbHostObject0;
 
 
 } SYSTEM_OBJECTS;
