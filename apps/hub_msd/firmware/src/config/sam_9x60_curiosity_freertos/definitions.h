91505a949e9f3f65c0e270480beacd9e6a51ad1d
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:29:16 +0530

    Regenerated demo for hub_msd_sam_9x60_curiosity_freertos
    MH3-107031

--- apps/hub_msd/firmware/src/config/sam_9x60_curiosity_freertos/definitions.h
+++ apps/hub_msd/firmware/src/config/sam_9x60_curiosity_freertos/definitions.h
@@ -48,11 +48,11 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/uhp/drv_usb_ehci.h"
-#include "driver/usb/uhp/drv_usb_ohci.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
 #include "usb/usb_host_hub.h"
+#include "driver/usb/uhp/drv_usb_ehci.h"
+#include "driver/usb/uhp/drv_usb_ohci.h"
 #include "usb/usb_msd.h"
 #include "usb/usb_host_msd.h"
 #include "usb/usb_host_scsi.h"
@@ -68,14 +68,14 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -89,13 +89,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "SAM9X60D1G"
-#define DEVICE_ARCH			 "ARM926EJ-S"
-#define DEVICE_FAMILY		 "SAM9"
-#define DEVICE_SERIES		 "SAM9X"
+#define DEVICE_NAME          "SAM9X60D1G"
+#define DEVICE_ARCH          "ARM926EJ-S"
+#define DEVICE_FAMILY        "SAM9"
+#define DEVICE_SERIES        "SAM9X6"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 600000000
+#define CPU_CLOCK_FREQUENCY 600000000U
 
 // *****************************************************************************
 // *****************************************************************************
@@ -207,10 +207,10 @@ Remarks:
 
 typedef struct
 {
+    SYS_MODULE_OBJ  usbHostObject0;
+
     SYS_MODULE_OBJ  drvUSBEHCIObject;
     SYS_MODULE_OBJ  drvUSBOHCIObject;
-
-    SYS_MODULE_OBJ  usbHostObject0;
 
     SYS_MODULE_OBJ  sysTime;
 
--- apps/hub_msd/firmware/src/config/sam_9x60_curiosity_freertos/tasks.c
+++ apps/hub_msd/firmware/src/config/sam_9x60_curiosity_freertos/tasks.c
@@ -60,6 +60,16 @@
 // Section: RTOS "Tasks" Routine
 // *****************************************************************************
 // *****************************************************************************
+static void F_USB_HOST_Tasks(  void *pvParameters  )
+{
+    while(true)
+    {
+        /* USB Host layer tasks routine */ 
+        USB_HOST_Tasks(sysObj.usbHostObject0);
+        vTaskDelay(1U / portTICK_PERIOD_MS);
+    }
+}
+
 static void F_DRV_USB_HOST_Tasks(  void *pvParameters  )
 {
     while(true)
@@ -68,16 +78,6 @@ static void F_DRV_USB_HOST_Tasks(  void *pvParameters  )
         DRV_USB_EHCI_Tasks(sysObj.drvUSBEHCIObject);
        /* USB OHCI Task Routine */
        DRV_USB_OHCI_Tasks(sysObj.drvUSBOHCIObject);
-        vTaskDelay(1U / portTICK_PERIOD_MS);
-    }
-}
-
-static void F_USB_HOST_Tasks(  void *pvParameters  )
-{
-    while(true)
-    {
-        /* USB Host layer tasks routine */ 
-        USB_HOST_Tasks(sysObj.usbHostObject0);
         vTaskDelay(1U / portTICK_PERIOD_MS);
     }
 }
@@ -93,8 +93,11 @@ static void lSYS_FS_Tasks(  void *pvParameters  )
 }
 
 
+
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
+
+
 
 static void lAPP_Tasks(  void *pvParameters  )
 {   
@@ -128,7 +131,7 @@ void SYS_Tasks ( void )
         "SYS_FS_TASKS",
         SYS_FS_STACK_SIZE,
         (void*)NULL,
-        SYS_FS_PRIORITY,
+        SYS_FS_PRIORITY ,
         (TaskHandle_t*)NULL
     );
 
@@ -138,18 +141,18 @@ void SYS_Tasks ( void )
     
 
     /* Maintain Middleware & Other Libraries */
-        /* Create OS Thread for USB Driver Tasks. */
-    (void) xTaskCreate( F_DRV_USB_HOST_Tasks,
-        "DRV_USB_HOST_TASKS",
+        /* Create OS Thread for USB_HOST_Tasks. */
+    (void) xTaskCreate( F_USB_HOST_Tasks,
+        "USB_HOST_TASKS",
         1024,
         (void*)NULL,
         1,
         (TaskHandle_t*)NULL
     );
 
-    /* Create OS Thread for USB_HOST_Tasks. */
-    (void) xTaskCreate( F_USB_HOST_Tasks,
-        "USB_HOST_TASKS",
+    /* Create OS Thread for USB Driver Tasks. */
+    (void) xTaskCreate( F_DRV_USB_HOST_Tasks,
+        "DRV_USB_HOST_TASKS",
         1024,
         (void*)NULL,
         1,
@@ -159,14 +162,15 @@ void SYS_Tasks ( void )
 
 
     /* Maintain the application's state machine. */
-        /* Create OS Thread for APP_Tasks. */
-    (void) xTaskCreate((TaskFunction_t) lAPP_Tasks,
-                "APP_Tasks",
-                1024,
-                NULL,
-                1,
-                &xAPP_Tasks);
-
+    
+    /* Create OS Thread for APP_Tasks. */
+    (void) xTaskCreate(
+           (TaskFunction_t) lAPP_Tasks,
+           "APP_Tasks",
+           1024,
+           NULL,
+           1U ,
+           &xAPP_Tasks);
 
 
 
