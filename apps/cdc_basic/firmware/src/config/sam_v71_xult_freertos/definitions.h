1fddaf641f8016ef7d1a1e218029ec50d9fa7be2
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 22:09:25 +0530

    Regenerated demo for cdc_basic_sam_v71_xult_freertos
    MH3-106863

--- apps/cdc_basic/firmware/src/config/sam_v71_xult_freertos/definitions.h
+++ apps/cdc_basic/firmware/src/config/sam_v71_xult_freertos/definitions.h
@@ -58,9 +58,9 @@
 #include "usb/usb_cdc.h"
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
@@ -79,13 +79,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMV71Q21B"
-#define DEVICE_ARCH			 "CORTEX-M7"
-#define DEVICE_FAMILY		 "SAMV"
-#define DEVICE_SERIES		 "SAMV71"
+#define DEVICE_NAME          "ATSAMV71Q21B"
+#define DEVICE_ARCH          "CORTEX-M7"
+#define DEVICE_FAMILY        "SAMV"
+#define DEVICE_SERIES        "SAMV71"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 300000000
+#define CPU_CLOCK_FREQUENCY 300000000U
 
 // *****************************************************************************
 // *****************************************************************************
