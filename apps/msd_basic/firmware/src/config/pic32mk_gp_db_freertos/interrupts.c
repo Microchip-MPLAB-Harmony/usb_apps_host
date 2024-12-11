0a373370ff56b5aeb8e4539b877179449950e2ea
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 22:13:52 +0530

    Regenerated demo for msd_basic_pic32mk_gp_db_freertos
    MH3-107077

--- apps/msd_basic/firmware/src/config/pic32mk_gp_db_freertos/interrupts.c
+++ apps/msd_basic/firmware/src/config/pic32mk_gp_db_freertos/interrupts.c
@@ -76,14 +76,14 @@ void USB_1_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void CORE_TIMER_Handler (void)
+void __attribute__((used)) CORE_TIMER_Handler (void)
 {
     CORE_TIMER_InterruptHandler();
 }
 
 
 
-void USB_1_Handler (void)
+void __attribute__((used)) USB_1_Handler (void)
 {
     DRV_USBFS_USB1_Handler();
 }
--- apps/msd_basic/firmware/src/config/pic32mk_gp_db_freertos/tasks.c
+++ apps/msd_basic/firmware/src/config/pic32mk_gp_db_freertos/tasks.c
@@ -81,8 +81,11 @@ static void lSYS_FS_Tasks(  void *pvParameters  )
 }
 
 
+
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
+
+
 
 static void lAPP_Tasks(  void *pvParameters  )
 {   
@@ -117,7 +120,7 @@ void SYS_Tasks ( void )
         "SYS_FS_TASKS",
         SYS_FS_STACK_SIZE,
         (void*)NULL,
-        SYS_FS_PRIORITY,
+        SYS_FS_PRIORITY ,
         (TaskHandle_t*)NULL
     );
 
@@ -140,14 +143,15 @@ void SYS_Tasks ( void )
 
 
     /* Maintain the application's state machine. */
-        /* Create OS Thread for APP_Tasks. */
-    (void) xTaskCreate((TaskFunction_t) lAPP_Tasks,
-                "APP_Tasks",
-                1024,
-                NULL,
-                1,
-                &xAPP_Tasks);
-
+    
+    /* Create OS Thread for APP_Tasks. */
+    (void) xTaskCreate(
+           (TaskFunction_t) lAPP_Tasks,
+           "APP_Tasks",
+           1024,
+           NULL,
+           1U ,
+           &xAPP_Tasks);
 
 
 
