f177d9d37624edf61de3ecdcc1b4726c30b7aca5
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:39:37 +0530

    Regenerated demo for hid_basic_keyboard_sam_a5d27_som1_ek_freertos
    MH3-106921

--- apps/hid_basic_keyboard/firmware/src/config/sam_a5d27_som1_ek_freertos/tasks.c
+++ apps/hid_basic_keyboard/firmware/src/config/sam_a5d27_som1_ek_freertos/tasks.c
@@ -60,6 +60,16 @@
 // Section: RTOS "Tasks" Routine
 // *****************************************************************************
 // *****************************************************************************
+static void F_USB_HOST_Tasks(  void *pvParameters  )
+{
+    while(true)
+    {
+        /* USB Host layer tasks routine */ 
+        USB_HOST_Tasks(sysObj.usbHostObject0);
+        vTaskDelay(10U / portTICK_PERIOD_MS);
+    }
+}
+
 static void F_DRV_USB_HOST_Tasks(  void *pvParameters  )
 {
     while(true)
@@ -72,18 +82,11 @@ static void F_DRV_USB_HOST_Tasks(  void *pvParameters  )
     }
 }
 
-static void F_USB_HOST_Tasks(  void *pvParameters  )
-{
-    while(true)
-    {
-        /* USB Host layer tasks routine */ 
-        USB_HOST_Tasks(sysObj.usbHostObject0);
-        vTaskDelay(10U / portTICK_PERIOD_MS);
-    }
-}
 
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
+
+
 
 static void lAPP_Tasks(  void *pvParameters  )
 {   
@@ -118,18 +121,18 @@ void SYS_Tasks ( void )
     
 
     /* Maintain Middleware & Other Libraries */
-        /* Create OS Thread for USB Driver Tasks. */
-    (void) xTaskCreate( F_DRV_USB_HOST_Tasks,
-        "DRV_USB_HOST_TASKS",
+        /* Create OS Thread for USB_HOST_Tasks. */
+    (void) xTaskCreate( F_USB_HOST_Tasks,
+        "USB_HOST_TASKS",
         1024,
         (void*)NULL,
         1,
         (TaskHandle_t*)NULL
     );
 
-    /* Create OS Thread for USB_HOST_Tasks. */
-    (void) xTaskCreate( F_USB_HOST_Tasks,
-        "USB_HOST_TASKS",
+    /* Create OS Thread for USB Driver Tasks. */
+    (void) xTaskCreate( F_DRV_USB_HOST_Tasks,
+        "DRV_USB_HOST_TASKS",
         1024,
         (void*)NULL,
         1,
@@ -139,14 +142,15 @@ void SYS_Tasks ( void )
 
 
     /* Maintain the application's state machine. */
-        /* Create OS Thread for APP_Tasks. */
-    (void) xTaskCreate((TaskFunction_t) lAPP_Tasks,
-                "APP_Tasks",
-                1024,
-                NULL,
-                1,
-                &xAPP_Tasks);
-
+    
+    /* Create OS Thread for APP_Tasks. */
+    (void) xTaskCreate(
+           (TaskFunction_t) lAPP_Tasks,
+           "APP_Tasks",
+           1024,
+           NULL,
+           1U ,
+           &xAPP_Tasks);
 
 
 
