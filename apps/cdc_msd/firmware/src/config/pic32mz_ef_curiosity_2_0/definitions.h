040e1af12089f2bfe802586737fcea55dce8e870
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:20:43 +0530

    Regenerated demo for cdc_msd_pic32mz_ef_curiosity_2_0
    MH3-106881

--- apps/cdc_msd/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
+++ apps/cdc_msd/firmware/src/config/pic32mz_ef_curiosity_2_0/definitions.h
@@ -50,8 +50,8 @@
 #include <stdbool.h>
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
-#include "driver/usb/usbhs/drv_usbhs.h"
 #include "peripheral/coretimer/plib_coretimer.h"
+#include "driver/usb/usbhs/drv_usbhs.h"
 #include "usb/usb_msd.h"
 #include "usb/usb_host_msd.h"
 #include "usb/usb_host_scsi.h"
@@ -68,11 +68,11 @@
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -86,13 +86,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MZ2048EFM144"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MZEF"
-#define DEVICE_SERIES		 "PIC32MZ"
+#define DEVICE_NAME          "PIC32MZ2048EFM144"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MZEF"
+#define DEVICE_SERIES        "PIC32MZ"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 198000000
+#define CPU_CLOCK_FREQUENCY 198000000U
 
 // *****************************************************************************
 // *****************************************************************************
