c5a9a567cc5bffe82056413041361cc05c1cadbd
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:39:39 +0530

    Regenerated demo for hid_basic_mouse_usart_sam_a5d27_som1_ek_freertos
    MH3-106977

--- apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d27_som1_ek_freertos/tasks.c
+++ apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d27_som1_ek_freertos/tasks.c
@@ -82,8 +82,11 @@ static void F_DRV_USB_HOST_Tasks(  void *pvParameters  )
     }
 }
 
+
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
+
+
 
 static void lAPP_Tasks(  void *pvParameters  )
 {   
@@ -139,14 +142,15 @@ void SYS_Tasks ( void )
 
 
     /* Maintain the application's state machine. */
-        /* Create OS Thread for APP_Tasks. */
-    (void) xTaskCreate((TaskFunction_t) lAPP_Tasks,
-                "APP_Tasks",
-                1024,
-                NULL,
-                1,
-                &xAPP_Tasks);
-
+    
+    /* Create OS Thread for APP_Tasks. */
+    (void) xTaskCreate(
+           (TaskFunction_t) lAPP_Tasks,
+           "APP_Tasks",
+           1024,
+           NULL,
+           1U ,
+           &xAPP_Tasks);
 
 
 
