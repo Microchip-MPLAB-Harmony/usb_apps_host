354370c9a286714e3c54eab0b2caec16282f6a38
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:42:21 +0530

    Regenerated demo for msd_basic_sam_a5d27_wlsom1_ek1
    MH3-107107

--- apps/msd_basic/firmware/src/config/sam_a5d27_wlsom1_ek1/definitions.h
+++ apps/msd_basic/firmware/src/config/sam_a5d27_wlsom1_ek1/definitions.h
@@ -68,11 +68,11 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
 #include "peripheral/tc/plib_tc0.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -86,13 +86,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMA5D27CLD2G"
-#define DEVICE_ARCH			 "CORTEX-A5"
-#define DEVICE_FAMILY		 "SAMA5"
-#define DEVICE_SERIES		 "SAMA5D2"
+#define DEVICE_NAME          "ATSAMA5D27CLD2G"
+#define DEVICE_ARCH          "CORTEX-A5"
+#define DEVICE_FAMILY        "SAMA5"
+#define DEVICE_SERIES        "SAMA5D2"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 492000000
+#define CPU_CLOCK_FREQUENCY 492000000U
 
 // *****************************************************************************
 // *****************************************************************************
