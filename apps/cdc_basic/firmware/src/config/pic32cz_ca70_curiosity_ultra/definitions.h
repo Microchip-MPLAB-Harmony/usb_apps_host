4a78d1565423cb8db166b5aae2b7f0fde6a3d59b
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:10:14 +0530

    Regenerated demo for cdc_basic_pic32cz_ca70_curiosity_ultra
    MH3-106807

--- apps/cdc_basic/firmware/src/config/pic32cz_ca70_curiosity_ultra/definitions.h
+++ apps/cdc_basic/firmware/src/config/pic32cz_ca70_curiosity_ultra/definitions.h
@@ -48,22 +48,22 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "driver/usb/usbhsv1/drv_usbhsv1.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "usb/usb_host_cdc.h"
 #include "usb/usb_cdc.h"
 #include "peripheral/efc/plib_efc.h"
-#include "bsp/bsp.h"
 #include "peripheral/tc/plib_tc0.h"
+#include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -195,9 +195,9 @@ Remarks:
 
 typedef struct
 {
-    SYS_MODULE_OBJ  drvUSBHSV1Object;
-
     SYS_MODULE_OBJ  usbHostObject0;
+
+    SYS_MODULE_OBJ  drvUSBHSV1Object;
 
     SYS_MODULE_OBJ  sysTime;
 
