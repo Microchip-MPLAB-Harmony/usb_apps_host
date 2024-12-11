22b037aa0de0a0babfb1a2f4299a1837ec3a0f84
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:53:36 +0530

    Regenerated demo for msd_basic_sam_a7d65_curiosity
    MH3-107117

--- apps/msd_basic/firmware/src/config/sam_a7d65_curiosity/initialization.c
+++ apps/msd_basic/firmware/src/config/sam_a7d65_curiosity/initialization.c
@@ -62,9 +62,10 @@
 // *****************************************************************************
 // *****************************************************************************
 /* Following MISRA-C rules are deviated in the below code block */
-/* MISRA C-2012 Rule 11.1 */
-/* MISRA C-2012 Rule 11.3 */
-/* MISRA C-2012 Rule 11.8 */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
 
 
 
@@ -96,13 +97,13 @@ static void DRV_USB_VBUSPowerEnable(uint8_t port, bool enable)
     {
         /* Enable the VBUS */
         VBUS_AH_PC20_PowerEnable(); 
-        VBUS_AH_PC21_PowerEnable();
+        VBUS_AH_PC21_PowerEnable(); 
     }
     else
     {
         /* Disable the VBUS */
-        VBUS_AH_PC20_PowerDisable();
-        VBUS_AH_PC21_PowerDisable();
+        VBUS_AH_PC20_PowerDisable(); 
+        VBUS_AH_PC21_PowerDisable(); 		
     }
 }
 
@@ -275,7 +276,6 @@ void SYS_Initialize ( void* data )
      
     
 	BSP_Initialize();
-
 
     /* MISRAC 2012 deviation block start */
     /* Following MISRA-C rules deviated in this block  */
