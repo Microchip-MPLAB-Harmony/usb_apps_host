4f3cbf518927f5ff251dbef765ca23f75f3ea08c
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:44:06 +0530

    Regenerated demo for cdc_basic_sam_a5d27_wlsom1_ek1_freertos
    MH3-106839

--- apps/cdc_basic/firmware/src/config/sam_a5d27_wlsom1_ek1_freertos/definitions.h
+++ apps/cdc_basic/firmware/src/config/sam_a5d27_wlsom1_ek1_freertos/definitions.h
@@ -48,10 +48,10 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usb/uhp/drv_usb_ehci.h"
-#include "driver/usb/uhp/drv_usb_ohci.h"
 #include "usb/usb_chapter_9.h"
 #include "usb/usb_host.h"
+#include "driver/usb/uhp/drv_usb_ehci.h"
+#include "driver/usb/uhp/drv_usb_ohci.h"
 #include "bsp/bsp.h"
 #include "peripheral/pit/plib_pit.h"
 #include "peripheral/mmu/plib_mmu.h"
@@ -82,13 +82,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMA5D27CLD2G"
-#define DEVICE_ARCH			 "CORTEX-A5"
-#define DEVICE_FAMILY		 "SAMA5"
-#define DEVICE_SERIES		 "SAMA5D2"
+#define DEVICE_NAME          "ATSAMA5D27CLD2G"
+#define DEVICE_ARCH          "CORTEX-A5"
+#define DEVICE_FAMILY        "SAMA5"
+#define DEVICE_SERIES        "SAMA5D2"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 492000000
+#define CPU_CLOCK_FREQUENCY 492000000U
 
 // *****************************************************************************
 // *****************************************************************************
@@ -200,10 +200,10 @@ Remarks:
 
 typedef struct
 {
+    SYS_MODULE_OBJ  usbHostObject0;
+
     SYS_MODULE_OBJ  drvUSBEHCIObject;
     SYS_MODULE_OBJ  drvUSBOHCIObject;
-
-    SYS_MODULE_OBJ  usbHostObject0;
 
     SYS_MODULE_OBJ  sysTime;
 
