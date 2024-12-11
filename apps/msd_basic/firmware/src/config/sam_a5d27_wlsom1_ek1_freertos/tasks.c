5e5d09b0f66c2160a8bacb98aa6709c0e6355d9a
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:44:13 +0530

    Regenerated demo for msd_basic_sam_a5d27_wlsom1_ek1_freertos
    MH3-107109

--- apps/msd_basic/firmware/src/config/sam_a5d27_wlsom1_ek1_freertos/tasks.c
+++ apps/msd_basic/firmware/src/config/sam_a5d27_wlsom1_ek1_freertos/tasks.c
@@ -93,8 +93,11 @@ static void lSYS_FS_Tasks(  void *pvParameters  )
 }
 
 
+
 /* Handle for the APP_Tasks. */
 TaskHandle_t xAPP_Tasks;
+
+
 
 static void lAPP_Tasks(  void *pvParameters  )
 {   
@@ -128,7 +131,7 @@ void SYS_Tasks ( void )
         "SYS_FS_TASKS",
         SYS_FS_STACK_SIZE,
         (void*)NULL,
-        SYS_FS_PRIORITY,
+        SYS_FS_PRIORITY ,
         (TaskHandle_t*)NULL
     );
 
@@ -159,14 +162,15 @@ void SYS_Tasks ( void )
 
 
     /* Maintain the application's state machine. */
-        /* Create OS Thread for APP_Tasks. */
-    (void) xTaskCreate((TaskFunction_t) lAPP_Tasks,
-                "APP_Tasks",
-                1024,
-                NULL,
-                1,
-                &xAPP_Tasks);
-
+    
+    /* Create OS Thread for APP_Tasks. */
+    (void) xTaskCreate(
+           (TaskFunction_t) lAPP_Tasks,
+           "APP_Tasks",
+           1024,
+           NULL,
+           1U ,
+           &xAPP_Tasks);
 
 
 
