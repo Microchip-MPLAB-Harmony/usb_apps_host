079c8bfa8ec82655f8e26aff80f78ca981d748dc
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 22:17:04 +0530

    Regenerated demo for msd_basic_sam_l21_xpro
    MH3-107133

--- apps/msd_basic/firmware/src/config/sam_l21_xpro/definitions.h
+++ apps/msd_basic/firmware/src/config/sam_l21_xpro/definitions.h
@@ -68,10 +68,10 @@
 #include "system/fs/fat_fs/file_system/ffconf.h"
 #include "system/fs/fat_fs/hardware_access/diskio.h"
 #include "driver/usb/usbfsv1/drv_usbfsv1.h"
-#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "bsp/bsp.h"
 #include "app.h"
 
 
@@ -85,13 +85,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAML21J18B"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAML"
-#define DEVICE_SERIES		 "SAML21"
+#define DEVICE_NAME          "ATSAML21J18B"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAML"
+#define DEVICE_SERIES        "SAML21"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
