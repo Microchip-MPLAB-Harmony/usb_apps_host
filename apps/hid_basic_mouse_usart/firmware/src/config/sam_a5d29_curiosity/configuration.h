e7f07ad3b34b3ed1bdbfc6bbc705dc942a21edb0
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2024-12-11 21:46:31 +0530

    Regenerated demo for hid_basic_mouse_usart_sam_a5d29_curiosity
    MH3-106983

--- apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d29_curiosity/configuration.h
+++ apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d29_curiosity/configuration.h
@@ -85,7 +85,7 @@ extern "C" {
 #define SYS_TIME_HW_COUNTER_WIDTH                   (32)
 #define SYS_TIME_HW_COUNTER_PERIOD                  (4294967295U)
 #define SYS_TIME_HW_COUNTER_HALF_PERIOD             (SYS_TIME_HW_COUNTER_PERIOD>>1)
-#define SYS_TIME_CPU_CLOCK_FREQUENCY                (498000000)
+#define SYS_TIME_CPU_CLOCK_FREQUENCY                (996000000)
 #define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES    (2200)
 
 
@@ -109,31 +109,6 @@ extern "C" {
 // Section: Middleware & Other Library Configuration
 // *****************************************************************************
 // *****************************************************************************
-// *****************************************************************************
-// *****************************************************************************
-// Section: USB Host Layer Configuration
-// *****************************************************************************
-// **************************************************************************
-
-/* Number of Endpoints used */
-
-/* Total number of devices to be supported */
-#define USB_HOST_DEVICES_NUMBER                             1U
-
-/* Target peripheral list entries */
-#define  USB_HOST_TPL_ENTRIES                               1 
-
-/* Maximum number of configurations supported per device */
-#define USB_HOST_DEVICE_INTERFACES_NUMBER                   5    
-
-#define USB_HOST_CONTROLLERS_NUMBER                         2U
-
-#define USB_HOST_TRANSFERS_NUMBER                           10U
-
-/* Provides Host pipes number */
-#define USB_HOST_PIPES_NUMBER                               10U
-
-
     
 /*** USB EHCI Driver Configurations ***/
 
@@ -176,6 +151,31 @@ extern "C" {
 #ifndef USB_ALIGN
 #define USB_ALIGN __ALIGNED(32)
 #endif 
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: USB Host Layer Configuration
+// *****************************************************************************
+// **************************************************************************
+
+/* Number of Endpoints used */
+
+/* Total number of devices to be supported */
+#define USB_HOST_DEVICES_NUMBER                             1U
+
+/* Target peripheral list entries */
+#define  USB_HOST_TPL_ENTRIES                               1 
+
+/* Maximum number of configurations supported per device */
+#define USB_HOST_DEVICE_INTERFACES_NUMBER                   5    
+
+#define USB_HOST_CONTROLLERS_NUMBER                         2U
+
+#define USB_HOST_TRANSFERS_NUMBER                           10U
+
+/* Provides Host pipes number */
+#define USB_HOST_PIPES_NUMBER                               10U
+
 
 /* Number of HID Client driver instances in the application */
 #define USB_HOST_HID_INSTANCES_NUMBER        1U
--- apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d29_curiosity/definitions.h
+++ apps/hid_basic_mouse_usart/firmware/src/config/sam_a5d29_curiosity/definitions.h
@@ -56,10 +56,10 @@
 #include "peripheral/aic/plib_aic.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
-#include "usb/usb_chapter_9.h"
-#include "usb/usb_host.h"
 #include "driver/usb/uhp/drv_usb_ehci.h"
 #include "driver/usb/uhp/drv_usb_ohci.h"
+#include "usb/usb_chapter_9.h"
+#include "usb/usb_host.h"
 #include "driver/usart/drv_usart.h"
 #include "peripheral/uart/plib_uart0.h"
 #include "usb/usb_host_hid.h"
@@ -82,13 +82,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMA5D29"
-#define DEVICE_ARCH			 "CORTEX-A5"
-#define DEVICE_FAMILY		 "SAMA"
-#define DEVICE_SERIES		 "SAMA5D"
+#define DEVICE_NAME          "ATSAMA5D29"
+#define DEVICE_ARCH          "CORTEX-A5"
+#define DEVICE_FAMILY        "SAMA"
+#define DEVICE_SERIES        "SAMA5D2"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 498000000
+#define CPU_CLOCK_FREQUENCY 996000000U
 
 // *****************************************************************************
 // *****************************************************************************
@@ -202,10 +202,10 @@ typedef struct
 {
     SYS_MODULE_OBJ  drvUsart0;
     SYS_MODULE_OBJ  sysTime;
-    SYS_MODULE_OBJ  usbHostObject0;
-
     SYS_MODULE_OBJ  drvUSBEHCIObject;
     SYS_MODULE_OBJ  drvUSBOHCIObject;
+
+    SYS_MODULE_OBJ  usbHostObject0;
 
 
 } SYSTEM_OBJECTS;
