1cbf22161ccad01e71823bb79f098ffce2754e8f
Karthikeya R Shervegar <Karthikeyar.Shervegar@microchip.com>
2025-03-20 11:55:23 +0530

    Regenerated demo for hid_basic_mouse_usart_pic32wm_bz6_curiosity
    MH3-111482

--- apps/hid_basic_mouse_usart/firmware/src/config/pic32wm_bz6_curiosity/initialization.c
+++ apps/hid_basic_mouse_usart/firmware/src/config/pic32wm_bz6_curiosity/initialization.c
@@ -341,9 +341,9 @@ void SYS_Initialize ( void* data )
 
 	GPIO_Initialize();
 
-    EVSYS_Initialize();
-
     SERCOM0_USART_Initialize();
+
+    EVSYS_Initialize();
 
 	BSP_Initialize();
     RTC_Initialize();
